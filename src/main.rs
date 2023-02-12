e4648061dc1c4792a89e2ef19213585a217185a1
MrEnder0 <endermobtrey@gmail.com>
2023-02-12 14:39:39 -0600

    base

---
+++ src/main.rs
@@ -0,0 +1,19 @@
+use std::{thread, time, io};
+
+fn main() {
+    let mut percent: i32 = 0;
+
+    println!("Enter time in milliseconds: ");
+    let mut time = String::new();
+    io::stdin().read_line(&mut time).expect("Failed to read line");
+    let time: u64 = time.trim().parse().expect("Please type a number!");
+
+
+    print!("Loading {}%", percent);
+
+    while percent < 100 {
+        percent += 1;
+        println!("\rLoading {}% \x1B[1A", percent);
+        thread::sleep(time::Duration::from_millis(time/100));
+    }
+}
