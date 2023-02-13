113ae44370fa08a34fb169f10e844cd0b60c8419
MrEnder0 <endermobtrey@gmail.com>
2023-02-13 17:32:01 -0600

    made color system better

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,20 @@
 use std::{thread, time, io};
 
 fn loading_bar(time: u64) {
+    let _color_red = "\x1B[31m";
+    let _color_green = "\x1B[32m";
+    let _color_reset = "\x1B[0m";
     let mut percent: i32 = 0;
 
-    print!("Loading {} (0%)", "-".repeat(10));
+    print!("Loading |{}{}{}| (0%)", _color_red, "█".repeat(10), _color_reset);
 
     while percent < 100 {
         percent += 1;
-        println!("\rLoading {}{} ({}%)\x1B[1A", "=".repeat((percent / 10).try_into().unwrap()), "-".repeat((10 - (percent / 10)).try_into().unwrap()), percent);
+        println!("\rLoading |{}{}{}{}{}| ({}%)\x1B[1A", _color_green, "█".repeat((percent / 10).try_into().unwrap()), _color_red, "█".repeat((10 - (percent / 10)).try_into().unwrap()), _color_reset, percent);
         thread::sleep(time::Duration::from_millis(time/100));
     }
 
-    println!("\rLoading {} (100%)", "=".repeat(10));
+    println!("\rLoading |{}{}{}| (100%)", _color_green, "█".repeat(10), _color_reset);
     thread::sleep(time::Duration::from_millis(100));
 }
 fn main() {
