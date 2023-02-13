b71dda46bcb1e56d3fa63ea6187b05b2418eb200
MrEnder0 <endermobtrey@gmail.com>
2023-02-13 16:39:27 -0600

    split into seperate funcion

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,25 @@
 use std::{thread, time, io};
 
-fn main() {
+fn loading_bar(time: u64) {
     let mut percent: i32 = 0;
 
-    println!("Enter time in milliseconds: ");
-    let mut time = String::new();
-    io::stdin().read_line(&mut time).expect("Failed to read line");
-    let time: u64 = time.trim().parse().expect("Please type a number!");
-
-
-    print!("Loading ----- (0%)");
+    print!("Loading {} (0%)", "-".repeat(10));
 
     while percent < 100 {
         percent += 1;
-        println!("\rLoading {}{} ({}%)\x1B[1A", "=".repeat((percent / 20).try_into().unwrap()), "-".repeat((5 - (percent / 20)).try_into().unwrap()), percent);
+        println!("\rLoading {}{} ({}%)\x1B[1A", "=".repeat((percent / 10).try_into().unwrap()), "-".repeat((10 - (percent / 10)).try_into().unwrap()), percent);
         thread::sleep(time::Duration::from_millis(time/100));
     }
 
-    println!("\rLoading {} (100%)", "=".repeat(5));
+    println!("\rLoading {} (100%)", "=".repeat(10));
     thread::sleep(time::Duration::from_millis(100));
+}
+fn main() {
+    println!("Enter time in milliseconds: ");
+    let mut time = String::new();
+
+    io::stdin().read_line(&mut time).expect("Failed to read line");
+    let time: u64 = time.trim().parse().expect("Please type a number!");
+    
+    loading_bar(time);
 }
