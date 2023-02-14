dc849d784d63a6b5ed09393820d042f6df984251
Mr.Ender <endermobtrey@gmail.com>
2023-02-14 21:57:13 +0000

    added title input to loading-bar function

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ pub mod colors;
 use std::{thread, time, io};
 use crate::colors::*;
 
-fn loading_bar(time: u64) {
+fn loading_bar(time: u64, title: &str) {
     let mut percent: i32 = 0;
 
-    print!("Loading |{}{}{}| (0%)", COLOR_RED, "█".repeat(10), COLOR_RESET);
+    print!("{} |{}{}{}| (0%)", title, COLOR_RED, "█".repeat(10), COLOR_RESET);
 
     while percent < 100 {
         percent += 1;
-        println!("\rLoading |{}{}{}{}{}| ({}%)\x1B[1A", COLOR_GREEN, "█".repeat((percent / 10) as usize), COLOR_RED, "█".repeat((10 - (percent / 10)) as usize), COLOR_RESET, percent);
+        println!("\r{} |{}{}{}{}{}| ({}%)\x1B[1A", title, COLOR_GREEN, "█".repeat((percent / 10) as usize), COLOR_RED, "█".repeat((10 - (percent / 10)) as usize), COLOR_RESET, percent);
         thread::sleep(time::Duration::from_millis(time/100));
     }
 
-    println!("\rLoading |{}{}{}| (Done {}✓{})", COLOR_GREEN, "█".repeat(10), COLOR_RESET, COLOR_GREEN, COLOR_RESET);
+    println!("\r{} |{}{}{}| (Done {}✓{})", title, COLOR_GREEN, "█".repeat(10), COLOR_RESET, COLOR_GREEN, COLOR_RESET);
     thread::sleep(time::Duration::from_millis(100));
 }
 fn main() {
@@ -24,5 +24,5 @@ fn main() {
     io::stdin().read_line(&mut time).expect("Failed to read line");
     let time: u64 = time.trim().parse().expect("Please type a number!");
     
-    loading_bar(time);
+    loading_bar(time, "Loading");
 }
