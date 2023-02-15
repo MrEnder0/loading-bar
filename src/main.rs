602cad89bd86fa6797df9a45d275d92c4a3ddd62
MrEnder0 <endermobtrey@gmail.com>
2023-02-14 21:14:05 -0600

    way more accurate way of time

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 pub mod colors;
 
-use std::{thread, time, io};
+use std::{time::{self, SystemTime}, thread, io};
 use crate::colors::*;
 
-fn loading_bar(time: u64, title: &str) {
+fn loading_bar(time: u128, now: SystemTime, title: &str) {
     let mut percent: i32 = 0;
 
     print!("{} |{}{}{}| (0%)", title, COLOR_RED, "█".repeat(10), COLOR_RESET);
 
     while percent < 100 {
-        percent += 1;
+        let elapsed = now.elapsed().unwrap().as_millis();
+        percent = (elapsed * 100 / time) as i32;
         println!("\r{} |{}{}{}{}{}| ({}%)\x1B[1A", title, COLOR_GREEN, "█".repeat((percent / 10) as usize), COLOR_RED, "█".repeat((10 - (percent / 10)) as usize), COLOR_RESET, percent);
-        thread::sleep(time::Duration::from_millis(time/100));
+        thread::sleep(time::Duration::from_millis((time/100) as u64));
     }
 
     println!("\r{} |{}{}{}| (Done {}✓{})", title, COLOR_GREEN, "█".repeat(10), COLOR_RESET, COLOR_GREEN, COLOR_RESET);
@@ -23,6 +24,9 @@ fn main() {
 
     io::stdin().read_line(&mut time).expect("Failed to read line");
     let time: u64 = time.trim().parse().expect("Please type a number!");
+
+    //get current time in milliseconds
+    let now = time::SystemTime::now();
     
-    loading_bar(time, "Loading");
+    loading_bar(time as u128, now, "Loading");
 }
