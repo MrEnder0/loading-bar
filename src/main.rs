4befb8bfd153e1b8ca87014a2b192f5ce3370fb5
MrEnder0 <endermobtrey@gmail.com>
2023-02-14 23:14:11 -0600

    switched to structs for loading bars

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,34 @@
 pub mod colors;
 
-use std::{time::{self, SystemTime}, thread, io};
+use std::{thread, time};
 use crate::colors::*;
 
-fn loading_bar(time: u128, now: SystemTime, title: &str) {
-    let mut percent: i32 = 0;
+struct LoadingBar {
+    progress: i32,
+    title: String
+}
 
-    print!("{} |{}{}{}| (0%)", title, COLOR_RED, "█".repeat(10), COLOR_RESET);
 
-    while percent < 100 {
-        let elapsed = now.elapsed().unwrap().as_millis();
-        percent = (elapsed * 100 / time) as i32;
-        println!("\r{} |{}{}{}{}{}| ({}%)\x1B[1A", title, COLOR_GREEN, "█".repeat((percent / 10) as usize), COLOR_RED, "█".repeat((10 - (percent / 10)) as usize), COLOR_RESET, percent);
-        thread::sleep(time::Duration::from_millis((time/100) as u64));
+fn progress_loading_bar(loading_bar: &mut LoadingBar, amount: i32) {
+    loading_bar.progress += amount
+}
+
+fn show_loading_bar(loading_bar: &mut LoadingBar) {
+    match loading_bar.progress {
+        100 => println!("\r{} |{}{}{}| (Done {}✓{})", loading_bar.title, COLOR_GREEN, "█".repeat(10), COLOR_RESET, COLOR_GREEN, COLOR_RESET),
+        _ => println!("\r{} |{}{}{}{}{}| ({}%)\x1B[1A", loading_bar.title, COLOR_GREEN, "█".repeat((loading_bar.progress / 10) as usize), COLOR_RED, "█".repeat((10 - (loading_bar.progress / 10)) as usize), COLOR_RESET, loading_bar.progress),
     }
-
-    println!("\r{} |{}{}{}| (Done {}✓{})", title, COLOR_GREEN, "█".repeat(10), COLOR_RESET, COLOR_GREEN, COLOR_RESET);
-    thread::sleep(time::Duration::from_millis(100));
 }
 fn main() {
-    println!("Enter time in milliseconds: ");
-    let mut time = String::new();
+    let mut progress_indicator = LoadingBar {
+        progress: 0,
+        title: "Loading".to_string()
+    };
 
-    io::stdin().read_line(&mut time).expect("Failed to read line");
-    let time: u64 = time.trim().parse().expect("Please type a number!");
-    
-    loading_bar(time as u128, time::SystemTime::now(), "Loading");
+    while progress_indicator.progress < 100 {
+        progress_loading_bar(&mut progress_indicator, 10);
+        show_loading_bar(&mut progress_indicator);
+        thread::sleep(time::Duration::from_millis(1000));
+    }
+    println!("");
 }
