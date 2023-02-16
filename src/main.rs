745ca5aed9df7cfc34fb763a4a24b2202e9545b6
MrEnder0 <endermobtrey@gmail.com>
2023-02-15 22:27:47 -0600

    added time linking back to loadingbar

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 pub mod colors;
 
-use std::{thread, time};
+use std::{thread, time::{self}};
 use crate::colors::*;
 
 struct LoadingBar {
@@ -10,12 +10,18 @@ struct LoadingBar {
 
 trait LoadingBarMethods {
     fn progress_loading_bar(&mut self, amount: i32);
+    fn set_loading_bar(&mut self, progress: i32);
     fn show_loading_bar(&mut self);
+    fn link_time_loading_bar(&mut self, time: u64);
 }
 
 impl LoadingBarMethods for LoadingBar {
     fn progress_loading_bar(&mut self, amount: i32) {
         self.progress += amount
+    }
+
+    fn set_loading_bar(&mut self, progress: i32) {
+        self.progress = progress
     }
 
     fn show_loading_bar(&mut self) {
@@ -24,14 +30,20 @@ impl LoadingBarMethods for LoadingBar {
             _ => println!("\r{} |{}{}{}{}{}| ({}%)\x1B[1A", self.title, COLOR_GREEN, "█".repeat((self.progress / 10) as usize), COLOR_RED, "█".repeat((10 - (self.progress / 10)) as usize), COLOR_RESET, self.progress),
         }
     }
+
+    fn link_time_loading_bar(&mut self, time: u64) {
+        let now = time::SystemTime::now();
+        while self.progress < 100 {
+            thread::sleep(time::Duration::from_millis(1 as u64));
+            let elapsed = now.elapsed().unwrap().as_millis();
+            self.progress = (elapsed * 100 / time as u128) as i32;
+            self.show_loading_bar();
+        }
+    }
 }
+
 fn main() {
     let mut some_data_progress = LoadingBar{ progress: 0, title: "Loading".to_string() };
 
-    while some_data_progress.progress < 100 {
-        some_data_progress.progress_loading_bar(10);
-        some_data_progress.show_loading_bar();
-        thread::sleep(time::Duration::from_millis(1000));
-    }
-    println!("");
+    some_data_progress.link_time_loading_bar(10000);
 }
